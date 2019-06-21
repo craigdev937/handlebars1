ab78be68d00a886849ae01c0305dc9e24fd3f510
indigomx9 <indigomx9@gmail.com>
2019-06-21 19:59:49 -0400

    Updated the template and controller.

--- src/controllers/hcontroller.ts
+++ src/controllers/hcontroller.ts
@@ -2,6 +2,11 @@ import express from "express";
 // Connection to the Model/Database.
 
 export const home = async (req, res: express.Response, next) => {
-    await res.render("index", { title: "Handlebars" });
+    await res.render("index", {
+        title: "Handlebars",
+        name: "Henry Quartermain",
+        age: 18,
+        isProfessor: true
+    });
 };
 
